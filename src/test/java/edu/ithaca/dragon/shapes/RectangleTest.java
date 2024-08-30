d1f839e171aadb0f6c228c0d4ad91e3518f3fa23
Erin <ecummings@ithaca.edu>
2024-08-29 20:45:53 -0400

    calcAreaTest

--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -9,4 +9,15 @@ import org.junit.jupiter.api.Test;
 
 public class RectangleTest {
     
+    @Test
+    public void calcAreaTest(){
+        Rectangle myRectangle = new Rectangle(12, 8);
+        assertEquals(96, myRectangle.calcArea());
+
+        myRectangle = new Rectangle(16, 10);
+        assertEquals(160, myRectangle.calcArea());
+
+        myRectangle = new Rectangle(.012, 7);
+        assertEquals(0.084, myRectangle.calcArea());
+    }
 }
