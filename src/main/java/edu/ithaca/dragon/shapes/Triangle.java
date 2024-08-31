6418cef6ef39bb39b150ec7bb724d468fb8dee33
Erin <ecummings@ithaca.edu>
2024-08-31 16:59:56 -0400

    Triangle class initial commit

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,11 @@
+package edu.ithaca.dragon.shapes;
+
+public class Triangle{
+    private double length;
+    private double width;
+
+    public Triangle(double lengthIn, double widthIn){
+        length = lengthIn;
+        width = widthIn;
+    }
+}
