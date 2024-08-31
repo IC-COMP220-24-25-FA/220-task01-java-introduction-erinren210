59b1feb027c833d0d446b1df8a8d490851bbf1e1
Erin <ecummings@ithaca.edu>
2024-08-31 17:10:46 -0400

    calcAreaTest

---
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -0,0 +1,22 @@
+package edu.ithaca.dragon.shapes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TriangleTest{
+
+    @Test
+    public void calcAreaTest(){
+        Triangle myTriangle = new Triangle(4,4);
+        assertEquals(8, myTriangle.calcArea());
+
+        myTriangle = new Triangle(8,3);
+        assertEquals(12, myTriangle.calcArea());
+
+        myTriangle = new Triangle(3,8);
+        assertEquals(12, myTriangle.calcArea());
+
+    }
+}
